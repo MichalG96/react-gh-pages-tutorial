a886cec67674a53069fb418002ca898edf130c31
Michal Grochecki <michalgrochecki@gmail.com>
2021-12-22 20:49:21 +0100

    Add search box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from "react";
 import {CardList} from "./components/card-list/card-list";
+import {SearchBox} from "./components/search-box/search-box";
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -17,10 +19,18 @@ export default class App extends React.Component {
   }
 
   render() {
+    const {monsters, searchField} = this.state
+    const filteredMonsters  = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className='App'>
-        <CardList monsters={this.state.monsters}/>
-
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
